dfecd81aa1965e4f413dd37a78ff8c1e905679dc
AshutoshAvadhani <ava24ashutosh@gmail.com>
2025-12-29 12:50:16 -0500

    Setup  a basic Express server with the Test API endpoint.

---
+++ backendNode/server.js
@@ -0,0 +1,11 @@
+//const express = require('express');
+import express from 'express';
+const app = express();
+
+app.get("/api/test", (req, res) => {
+    res.json({ message: "Hello from the backend!" });
+});
+
+app.listen(3001, () => {
+    console.log("Server is running on http://localhost:3001")
+});
